8fae0f7ef48ef36728e484f35c8566a5519cbc37
Sash Zats <sash@zats.io>
2015-11-26 08:53:33 -0800

    Initial Commit

---
+++ Tribute/Tribute.h
@@ -0,0 +1,19 @@
+//
+//  Tribute.h
+//  Tribute
+//
+//  Created by Sash Zats on 11/26/15.
+//  Copyright © 2015 Sash Zats. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Tribute.
+FOUNDATION_EXPORT double TributeVersionNumber;
+
+//! Project version string for Tribute.
+FOUNDATION_EXPORT const unsigned char TributeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Tribute/PublicHeader.h>
+
+
