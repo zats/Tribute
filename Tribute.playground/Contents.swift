a89c4ad49741981c90038aa924005931cef0ecf3
Sash Zats <sash@zats.io>
2015-12-04 19:21:16 -0800

    Update playground

--- Tribute.playground/Contents.swift
+++ Tribute.playground/Contents.swift
@@ -2,14 +2,14 @@
 
 import Tribute
 
-let string = NSMutableAttributedString().add("Hello") {
-    $0.font = .systemFontOfSize(20)
-    $0.color = .blueColor()
-    $0.URL = NSURL(string: "http://google.com")!
-    $0.underline = .StyleSingle
-}.add(" world") {
-    $0.underline = nil
-    $0.color = nil
-}
+let image = [#Image(imageLiteral: "swift@2x.png")#]
 
+let string = NSMutableAttributedString()
+    .add("hello world") {
+        $0.underline = NSUnderlineStyle.StyleSingle
+        $0.color = .blueColor()
+    }
+    .add(image)
+    .add("hello world")
 string
+
