a820b5f96b4b701f0bc274d13b89cba86f512e60
Paul Chou <paul.chou.dev@gmail.com>
2016-10-17 23:22:07 +0800

    update color property by using #colorLiteral in playground

--- Tribute.playground/Contents.swift
+++ Tribute.playground/Contents.swift
@@ -7,23 +7,23 @@ import XCPlayground
 let string = NSMutableAttributedString()
     .add("Tribute is a ") {
         $0.font = .systemFontOfSize(15)
-        $0.color = [#Color(colorLiteralRed: 0.8504372835, green: 0.2181603462, blue: 0.1592026055, alpha: 1)#]
+        $0.color = #colorLiteral(red: 0.8504372835, green: 0.2181603462, blue: 0.1592026055, alpha: 1)
     }
     .add("micro") {
         $0.obliqueness = 0.2
         $0.underline = .StyleSingle
-        $0.color = [#Color(colorLiteralRed: 0.7608990073, green: 0.2564961016, blue: 0, alpha: 1)#]
+        $0.color = #colorLiteral(red: 0.7608990073, green: 0.2564961016, blue: 0, alpha: 1)
     }.add(" library") {
         $0.underline = nil
-        $0.color = [#Color(colorLiteralRed: 0.8526210189, green: 0.4221832156, blue: 0, alpha: 1)#]
+        $0.color = #colorLiteral(red: 0.8526210189, green: 0.4221832156, blue: 0, alpha: 1)
     }.add(" for ") {
         $0.obliqueness = nil
-        $0.color = [#Color(colorLiteralRed: 0.9767052531, green: 0.605463922, blue: 0, alpha: 1)#]
+        $0.color = #colorLiteral(red: 0.9767052531, green: 0.605463922, blue: 0, alpha: 1)
     }
-    .add([#Image(imageLiteral: "swift@2x.png")#], bounds: CGRect(x: 0, y: -8, width: 30, height: 30))
+    .add(#imageLiteral(resourceName: "swift@2x.png"), bounds: CGRect(x: 0, y: -8, width: 30, height: 30))
     .add("\nIt simplifies"){
         $0.reset()
-        $0.color = [#Color(colorLiteralRed: 0.5948907137, green: 0.7498663664, blue: 0, alpha: 1)#]
+        $0.color = #colorLiteral(red: 0.5948907137, green: 0.7498663664, blue: 0, alpha: 1)
         $0.alignment = .Center
     }
     .add(" ")
@@ -33,7 +33,7 @@ let string = NSMutableAttributedString()
     }.add("\nNSAttributedStrings") {
         $0.URL = nil
         $0.font = UIFont(name: "Courier", size: 14)
-        $0.color = [#Color(colorLiteralRed: 0.1819814891, green: 0.6942673326, blue: 0.5302479267, alpha: 1)#]
+        $0.color = #colorLiteral(red: 0.1819814891, green: 0.6942673326, blue: 0.5302479267, alpha: 1)
         $0.textEffect = .Letterpress
     }
 
@@ -58,6 +58,6 @@ let label = UILabel()
 label.attributedText = s
 label.numberOfLines = 0
 label.sizeToFit()
-label.backgroundColor = [#Color(colorLiteralRed: 0.9607843136999999, green: 0.9607843136999999, blue: 0.9607843136999999, alpha: 1)#]
+label.backgroundColor = #colorLiteral(red: 0.9607843136999999, green: 0.9607843136999999, blue: 0.9607843136999999, alpha: 1)
 page.liveView = label
 
